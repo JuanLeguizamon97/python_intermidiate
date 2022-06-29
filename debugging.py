e5f67356a50fd60322c0429786b031a651822e39
Juan Leguizamon <juanc.leguizamon@urosario.edu.co>
2022-06-29 11:03:36 -0500

    Corrección de posibles errores de codigo usando Try, Except y Raise

--- debugging.py
+++ debugging.py
@@ -1,15 +1,21 @@
 def divisors(num):
-    divisors = []
 
-    for i in range(1, num +1 ):
-        if num % i == 0:
-            divisors.append(i)
+    divisors = [i for i in range(1,num +1 ) if num % i == 0]
     return divisors
+    
 
 def run():
-    num = int(input("Please enter a number: "))
-    print(divisors(num))
-    print("Give me a challenge son!")
+    try:
+        num = int(input("Please enter a number: "))
+        if num <=0:
+           raise ValueError
+        print(divisors(num))
+        print("Give me a challenge son!")
+    
+    except ValueError:
+        print("Please enter a valid value.")
+    except Exception:
+        print("Please enter a valid number")
 
 if __name__ == "__main__":
     run()
